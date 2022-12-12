c9dfe6c9680d96acb8f5c02284cc27f05fbc02b3
Gtyp <gtyp_local@proton.me>
2022-12-12 17:00:23 +0200

    fix: fix string comparison

--- custom_triplets/x64-windows-rel.cmake
+++ custom_triplets/x64-windows-rel.cmake
@@ -3,6 +3,6 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
-if(${PORT} STREQUAL "qt5-base")
+if(PORT STREQUAL "qt5-base")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
